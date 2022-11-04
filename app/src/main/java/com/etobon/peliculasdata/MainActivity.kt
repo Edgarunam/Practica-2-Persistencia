72ab3fae7cbe493eb5dd198c372ab7e6227a759e
Edgarunam <etobon96@gmail.com>
2022-11-03 22:03:13 -0600

    APP crash

--- app/src/main/java/com/etobon/peliculasdata/MainActivity.kt
+++ app/src/main/java/com/etobon/peliculasdata/MainActivity.kt
@@ -3,9 +3,23 @@ package com.etobon.peliculasdata
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.room.Room
+
+
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
+
+
     }
+
+
+
+
 }
--- app/src/main/java/com/etobon/peliculasdata/entidadPelicula.kt
+++ app/src/main/java/com/etobon/peliculasdata/entidadPelicula.kt
@@ -8,10 +8,9 @@ import java.net.URI
 @Entity(tableName = "tabla_pelicula")
 data class entidadPelicula (
     @PrimaryKey val TituloPelicula:String,
-    @ColumnInfo(name="nombre_pelicula") val nombrePelicula:String?,
     @ColumnInfo(name = "director_pelicula") val directorPelicula:String?,
     @ColumnInfo(name = "genero_pelicula") val generoPelicula: String?,
     @ColumnInfo(name = "fechar_pelicula") val fechaPelicua:Int?,
-    @ColumnInfo(name = "calificacion_pelicula") val calificacionPelicula:Int?,
-    @ColumnInfo(name = "uri_Pelicula") val uriPelicula:URI?
+    @ColumnInfo(name = "calificacion_pelicula") val calificacionPelicula:Double?
+
         )
