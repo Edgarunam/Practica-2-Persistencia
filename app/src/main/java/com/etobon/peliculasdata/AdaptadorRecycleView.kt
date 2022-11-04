34c27b659f39843df4f99ad047a1f76f907762f6
Edgarunam <etobon96@gmail.com>
2022-11-03 21:28:38 -0600

    Adaptador-RecycleView Terminado

--- app/src/main/java/com/etobon/peliculasdata/AdaptadorRecycleView.kt
+++ app/src/main/java/com/etobon/peliculasdata/AdaptadorRecycleView.kt
@@ -1,4 +1,32 @@
 package com.etobon.peliculasdata
 
-class AdaptadorRecycleView {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+
+class AdaptadorRecycleView(private val peliculasrv: List<entidadPelicula>) :
+    RecyclerView.Adapter<AdaptadorRecycleView.ViewHolder>(){
+
+    class ViewHolder (val view:View):RecyclerView.ViewHolder(view)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.layout_plantilla_rv,parent,false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.view.findViewById<TextView>(R.id.tvTituloPelicula).text = peliculasrv[position].TituloPelicula
+        holder.view.findViewById<TextView>(R.id.tvDirectorPelicula).text = peliculasrv[position].directorPelicula
+        holder.view.findViewById<TextView>(R.id.tvGeneroPelicula).text = peliculasrv[position].generoPelicula
+        holder.view.findViewById<TextView>(R.id.tvFechaPelicula).text = peliculasrv[position].fechaPelicua.toString()
+        holder.view.findViewById<TextView>(R.id.tvCalificacion).text = peliculasrv[position].calificacionPelicula.toString()
+
+
+
+    }
+
+    override fun getItemCount(): Int = peliculasrv.size
+
 }
