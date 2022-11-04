326abf09bf4f4a71c8490d4716b1851670d08b75
Edgarunam <etobon96@gmail.com>
2022-11-04 00:17:38 -0600

    Spinner finally - InsterActivity

--- app/src/main/java/com/etobon/peliculasdata/InsertActivity.kt
+++ app/src/main/java/com/etobon/peliculasdata/InsertActivity.kt
@@ -2,10 +2,71 @@ package com.etobon.peliculasdata
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.AndroidException
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import android.widget.Toast
+import com.etobon.peliculasdata.databinding.ActivityInsertBinding
+import com.etobon.peliculasdata.databinding.ActivityMainBinding
 
 class InsertActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityInsertBinding
+    var generoMovie:String = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_insert)
+        binding = ActivityInsertBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        ArrayAdapter.createFromResource(this,R.array.Generos,android.R.layout.simple_spinner_item).also {
+            adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_item)
+            binding.spinnerGeneros.adapter = adapter
+
+        }
+        SpinnerSelection(binding.spinnerGeneros)
+
+
+    }
+
+    private fun SpinnerSelection(spinner: Spinner){
+        spinner.onItemSelectedListener = object  : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>, view: View?, pos: Int, id: Long) {
+                when (pos) {
+                    0 -> {
+                        generoMovie = "Acción"
+                    }
+                    1 -> {
+                        generoMovie = "Aventura"
+                    }
+                    2 -> {
+                        generoMovie = "Ciencia Ficción"
+                    }
+                    3 -> {
+                        generoMovie = "Comedia"
+                    }
+                    4 -> {
+                        generoMovie = "Drama"
+                    }
+                    5 -> {
+                        generoMovie = "Fantasía"
+                    }
+                    6 -> {
+                        generoMovie = "Musical"
+                    }
+                    7 -> {
+                        generoMovie = "Suspenso"
+                    }
+                    8 -> {
+                        generoMovie = "Terror"
+                    }
+                }
+
+            }
+            override fun onNothingSelected(parent: AdapterView<*>) {
+                generoMovie = "Sin Genero Seleccionado"
+            }
+        }
     }
 }
--- app/src/main/java/com/etobon/peliculasdata/MainActivity.kt
+++ app/src/main/java/com/etobon/peliculasdata/MainActivity.kt
@@ -3,22 +3,11 @@ package com.etobon.peliculasdata
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import androidx.room.Room
+
 import com.etobon.peliculasdata.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    /*
-    val moviesBD : AppDatabase by lazy {
-        Room.databaseBuilder( this, AppDatabase::class.java, "movies_bd")
-            .allowMainThreadQueries()
-            .fallbackToDestructiveMigration()
-            .build()
-    }
-
-     */
 
 
     private lateinit var binding: ActivityMainBinding
