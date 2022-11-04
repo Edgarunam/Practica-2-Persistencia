9b261bfc7acc8ea757a25e573eb71f87aa1c615f
Edgarunam <etobon96@gmail.com>
2022-11-03 22:53:56 -0600

    Binding + BD

--- app/src/main/java/com/etobon/peliculasdata/MainActivity.kt
+++ app/src/main/java/com/etobon/peliculasdata/MainActivity.kt
@@ -2,10 +2,31 @@ package com.etobon.peliculasdata
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
+import androidx.room.Room
+import com.etobon.peliculasdata.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    val moviesBD : AppDatabase by lazy {
+        Room.databaseBuilder( this, AppDatabase::class.java, "movies_bd")
+            .allowMainThreadQueries()
+            .fallbackToDestructiveMigration()
+            .build()
+    }
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+
+
+
+
+
+
+
     }
 }
